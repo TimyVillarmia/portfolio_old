0e99f3370c241e54ce671fd864ea390e7412dde7
Timy <timyvillarmia@gmail.com>
2023-12-29 19:43:04 +0800

    added: about me content

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,6 +15,9 @@ const projectsCollection = defineCollection({
 
     }),
 });
+
+
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
